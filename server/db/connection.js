8ad0282aad2fab513388c41bf7bb4e22bc4cdb03
Samiksha Mitra <isamikshasamiksha@gmail.com>
2026-01-14 01:16:09 +0530

    Apply local changes after rebase

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,7 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const connect = (url) =>{
-    return mongoose.connect(url)
-} 
+const connect = async () => {
+  try {
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
+      throw new Error("MONGO_URI not defined");
+    }
 
-module.exports = connect
+    await mongoose.connect(uri);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("invalid db connection");
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connect;
